a83fba53b4f8ce2981cdc0500256ca96fce6268e
ehgks0000 <ehgks0083@gmail.com>
2020-06-21 12:00:21 +0900

    3nd 포테이토 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Potato from "./Potato";
+
 // import logo from './logo.svg';
 // import "./App.css";
 
 function App() {
-  return <div className="App">helloooo!</div>;
+  return (
+    <div className="App">
+      <h1>helloooo</h1>
+      <Potato></Potato>
+    </div>
+  );
 }
 
 export default App;
