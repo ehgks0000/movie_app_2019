715229a4b2e4f511359cd7f752fd167c8a1a782f
ehgks0000 <ehgks0083@gmail.com>
2020-06-26 02:21:20 +0900

    axios설치

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   //constructor render componentDidMount
 
   constructor(props) {
     super(props);
-    console.log("hellow");
+    console.log("hh");
   }
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  // 리액트는 render를 리프레시 하지 않는다.
-  //setstate가 새로운 state와 render function을 호출함
-  // state를 바꿀려면 setstate 사용
-  add = () => {
-    //이렇게 사용하는거 추천하지 않음 멋지지 않음
-    // this.setState({ count: this.state.count + 1 });
-    this.setState((state) => ({ count: state.count + 1 }));
-  };
-  minus = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-  componentDidMount() {} // 랜더 된 후
+
+  // https://yts-proxy.now.sh/list_movies.json
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  } // 랜더 된 후
   componentDidUpdate() {} // 다시 랜더 된 후
   componentWillUnmount() {} //랜더가 종료 된 후
 
   render() {
-    return (
-      <div>
-        <h1>The num is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state; // es6 isLoading = this.state.isLoading 이거랑 같음
+    return <div>{isLoading ? "Loading" : "im ready"}</div>;
   }
 }
 export default App;
